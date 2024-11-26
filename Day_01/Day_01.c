0bda699b452f40bfc658ccf0ad9bf8320921b313
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-11-26 22:01:12 +0100

    Day 1 Part 1

--- Day_01/Day_01.c
+++ Day_01/Day_01.c
@@ -1,5 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("TEST");
+typedef struct
+{
+    int length;
+    int * inputs;
+} InputData;
+
+InputData
+readInput()
+{
+    FILE * inputFile = fopen("Input.txt", "r");
+    char line[256];
+    int * values = malloc(sizeof(int) * 1000);
+    int counter = 0;
+    InputData result;
+
+    if (inputFile != NULL)
+    {
+        while (fgets(line, sizeof(line), inputFile))
+        {
+            values[counter] = atoi(line);
+            counter++;
+        }
+        fclose(inputFile);
+        result.length = counter;
+        result.inputs = values;
+    }
+    else
+    {
+        fprintf(stderr, "Unable to open!\n");
+    }
+
+    return result;
+}
+
+int
+main()
+{
+    InputData inputData = readInput();
+
+    for (int i = 0; i < inputData.length; i++)
+    {
+        for (int j = i + 1; j < inputData.length; j++)
+        {
+            if (inputData.inputs[i] + inputData.inputs[j] == 2020)
+            {
+                printf("Result Part 1: %i", inputData.inputs[i] * inputData.inputs[j]);
+            }
+        }
+    }
+
+    free(inputData.inputs);
 }
